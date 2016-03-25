970ae56c132bc2a6d88c16f118225bff965b66d3
jeka911 <jeka911@gmail.com>
2016-03-25 02:25:17 +0200

    rfr -> bundled

--- includes/servers/public_server.js
+++ includes/servers/public_server.js
@@ -1,7 +1,9 @@
 var restify = require('restify');
+var rfr = require('rfr');
+var path = require('path');
 
 var server = restify.serveStatic({
-	directory: './public'
+	directory: path.join(rfr.root, '/public/')
 });
 
 module.exports = server;
