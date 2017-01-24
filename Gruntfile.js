3648c928ecd1b0a57e3ea9a6d911b28a6caa08ac
jeka911 <jeka911@gmail.com>
2017-01-24 10:45:42 +0200

    fix for node v 7.4.0, thanks to Shenggao Li

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,7 +106,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-node-inspector');
   grunt.loadNpmTasks('grunt-mocha-cli');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-run');
